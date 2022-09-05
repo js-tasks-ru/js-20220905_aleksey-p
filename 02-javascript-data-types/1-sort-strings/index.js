0e763eb8be27c29fb7a06b3743139660cbe16249
alexMercy <68870653+alexMercy@users.noreply.github.com>
2022-09-06 02:22:34 +0300

    complete sortStrings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,10 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(arr, param = 'asc') {
+  const arrCopy = [...arr];
+  let firstCase = param === 'asc' ? 'upper' : 'lower';
+  let sortType = param === 'asc' ? 1 : -1;
+  return arrCopy.sort((a, b) => sortType * a.localeCompare(b, ['ru', 'eng'], { caseFirst: firstCase}));
 }
