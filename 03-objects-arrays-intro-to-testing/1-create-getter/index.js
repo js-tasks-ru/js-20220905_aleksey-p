65ebe4d71b07c48431fa556c36e77817f16bf6b2
alexMercy <68870653+alexMercy@users.noreply.github.com>
2022-09-06 20:46:59 +0300

    complete getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -1,8 +1,8 @@
 /**
  * createGetter - creates function getter which allows select value from object
- * @param {string} path - the strings path separated by dot
+ * @param {string[]} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return (obj) => Object.keys(obj).length ? eval("obj." + path) : undefined;
 }
